7186e95bed4a950d3f21fc4193f94367740e1363
Casa Imaginada <casaimaginadacoop@gmail.com>
2026-04-11 12:53:01 -0300

    mejoro página 404

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     <article class="popover-hint">
       <h1>🌱 Esta semilla todavía está germinando</h1>
       <p>El contenido que buscás es parte del jardín de Casa Imaginada, pero todavía no tiene su página. Volvé pronto o explorá desde el inicio.</p>
-      <a href={baseDir}>Volver al inicio</a>
+      <a href="/casa-jardin/">Volver al jardín</a>
     </article>
   )
 }
